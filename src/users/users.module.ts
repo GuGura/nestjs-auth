7ccfb1e13dd88714a078b384ceafd0494d98e9b5
pjy <wodus331@vanillacode.net>
2023-11-03 19:05:07 +0900

    create test user

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AuthModule } from '../auth/auth.module';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [AuthModule],
+  controllers: [UsersController],
   providers: [UsersService],
 })
 export class UsersModule {}
